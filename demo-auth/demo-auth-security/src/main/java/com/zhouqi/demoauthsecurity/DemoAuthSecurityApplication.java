8e01ad7a3fd09bc1acbe0424e018705d697d0e92
zhouqi9 <zhouqi9@lenovo.com>
2024-06-03 11:40:51 +0800

    feat: 初始化auth中security

---
+++ demo-auth/demo-auth-security/src/main/java/com/zhouqi/demoauthsecurity/DemoAuthSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.zhouqi.demoauthsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAuthSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAuthSecurityApplication.class, args);
+    }
+
+}
---
+++ demo-auth/demo-auth-security/src/test/java/com/zhouqi/demoauthsecurity/DemoAuthSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhouqi.demoauthsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAuthSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
